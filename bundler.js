7b2278b2127fa71781f58a60eb1c3f369f63598d
Sing <sing.chen@linecorp.com>
2019-11-12 17:20:41 +0800

    Parse file with babel

--- bundler.js
+++ bundler.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
 const config = require('./webpack.config')
+const parser = require('@babel/parser')
+
 
 function createAsset(filePath) {
   const content = fs.readFileSync(filePath, 'utf-8')
-  return content
+
+  // See https://astexplorer.net/ for more detail
+  const ast = parser.parse(content, { sourceType: 'module' })
+
+  return ast
 }
 
 console.log(createAsset(config.entry))
