075a48a938b731c2a9dda616ba829da6b36827f8
Sing <sing.chen@linecorp.com>
2019-11-14 15:59:11 +0800

    Create Asset with dependencies

--- bundler.js
+++ bundler.js
@@ -1,15 +1,23 @@
 const fs = require('fs')
 const config = require('./webpack.config')
-const parser = require('@babel/parser')
-
+const { traverse, parse } = require('@babel/core')
 
 function createAsset(filePath) {
   const content = fs.readFileSync(filePath, 'utf-8')
+  const asset = {
+    filePath,
+    dependencies: []
+  }
 
   // See https://astexplorer.net/ for more detail
-  const ast = parser.parse(content, { sourceType: 'module' })
+  const ast = parse(content, { sourceType: 'module' })
+  traverse(ast, {
+    ImportDeclaration: (declaration) => {
+      asset.dependencies.push(declaration.node.source.value)
+    }
+  })
 
-  return ast
+  return asset
 }
 
 console.log(createAsset(config.entry))
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'hidden-source-map',
   entry: './src/entry.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
