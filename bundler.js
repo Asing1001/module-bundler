f59bf3621afce9e0353e1a8eebe0fd581328cacd
Sing <sing.chen@linecorp.com>
2019-11-15 19:33:59 +0800

    bundle the graph

--- bundler.js
+++ bundler.js
@@ -1,40 +1,71 @@
 const fs = require('fs')
 const config = require('./webpack.config')
-const { traverse, parse } = require('@babel/core')
+const { traverse, parse, transformSync } = require('@babel/core')
 const path = require('path')
 
-let ID = 0
 function createAsset(filePath) {
   const content = fs.readFileSync(filePath, 'utf-8')
-  const asset = {
-    id: ID++,
-    filePath,
-    dependencies: []
-  }
-
+  const dependencies = []
   // See https://astexplorer.net/ for more detail
   const ast = parse(content, { sourceType: 'module' })
   traverse(ast, {
     ImportDeclaration: (declaration) => {
-      asset.dependencies.push(declaration.node.source.value)
+      dependencies.push(declaration.node.source.value)
     }
   })
+  const { code } = transformSync(content, { presets: ['@babel/env'] })
 
-  return asset
+  return {
+    filePath,
+    dependencies,
+    code
+  }
 }
 
 function createGraph(filePath) {
   const graph = [createAsset(filePath)]
   for (const asset of graph) {
     const assetDir = path.dirname(asset.filePath)
-    asset.dependencies.forEach(dependency => {
-      const dependencyPath = path.join(assetDir, dependency)
-      graph.push(createAsset(dependencyPath))
+    asset.mapping = {}
+    asset.dependencies.forEach(relativePath => {
+      const dependencyPath = path.join(assetDir, relativePath)
+      const dependency = createAsset(dependencyPath)
+      asset.mapping[relativePath] = dependencyPath
+      graph.push(dependency)
     })
   }
   return graph
 }
 
+function bundle(filePath) {
+  const graph = createGraph(filePath)
+  let modules = ''
+  graph.forEach(module => {
+    modules += `${JSON.stringify(module.filePath)}: {
+      fn: function(require, module, exports) {
+        ${module.code}
+      },
+      mapping: ${JSON.stringify(module.mapping)}
+    },`
+  })
 
+  return `(function(modules){
+    function require(path){
+      const {fn, mapping} = modules[path]
+      const module = { exports: {} }
+      function localRequire(relativePath) {
+        return require(mapping[relativePath])
+      }
+      fn(localRequire, module, module.exports)
+      return module.exports
+    }
 
-console.log(createGraph(config.entry))
+    require(${JSON.stringify(filePath)})
+  })({${modules}})`
+
+}
+
+const bundleJS = bundle(config.entry)
+console.log(bundleJS)
+const outputFileName = path.join(config.output.path, config.output.filename)
+fs.writeFileSync(outputFileName, bundleJS)
