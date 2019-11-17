d8e33d26f062908ecc46c1a80fa9461e160097b9
Sing <sing.chen@linecorp.com>
2019-11-17 20:26:06 +0800

    Packing the result

--- bundler.js
+++ bundler.js
@@ -3,12 +3,9 @@ const path = require('path')
 const { entry, output } = require('./webpack.config')
 const { traverse, transformSync, parseSync } = require('@babel/core')
 
-
 let ID = 0
 
 const createModule = (filePath) => {
-  const id = ID++
-  const dependencyMap = {}
   const content = fs.readFileSync(filePath, 'utf8')
   const { code } = transformSync(content, { presets: ['@babel/env'] })
   const ast = parseSync(content, { sourceType: 'module' })
@@ -20,44 +17,58 @@ const createModule = (filePath) => {
   })
 
   return {
-    id,
+    id: ID++,
     filePath,
     code,
     dependencies,
-    dependencyMap,
-    toString: () => `${id}: {
-      factory: (require, module, exports) => {
-        ${code}
-      },
-      dependencyMap: ${JSON.stringify(dependencyMap)}
-    }`
+    dependencyMap: {},
   }
 }
+
+const stringifyModule = (module) => `${module.id}: {
+  factory: (require, module, exports) => {
+    ${module.code}
+  },
+  dependencyMap: ${JSON.stringify(module.dependencyMap)}
+}`
 
 const resolveDependencyPath = (module, dependency) => {
   const dirname = path.dirname(module.filePath)
   return path.join(dirname, dependency)
 }
 
-const createGraph = (filePath) => {
-  const graph = [createModule(filePath)]
-  for (const module of graph) {
+const resolveModules = (filePath) => {
+  const modules = [createModule(filePath)]
+  for (const module of modules) {
     module.dependencies.forEach(dependency => {
       const dependencyPath = resolveDependencyPath(module, dependency)
       const childModule = createModule(dependencyPath)
       module.dependencyMap[dependency] = childModule.id
-      graph.push(childModule)
+      modules.push(childModule)
     })
   }
-  return graph
-}
-
-const packing = (filePath) => {
-  const graph = createGraph(filePath)
-  const modules = graph.map(module => module.toString()).join(',')
   return modules
 }
 
+const packing = (filePath) => {
+  const modules = resolveModules(filePath)
+  const modulesString = modules.map(stringifyModule).join(',')
+
+  return `(function(modules) {
+    function require(id) {
+      const { factory, dependencyMap } = modules[id]
+      function localRequire (dependency) {
+        return require(dependencyMap[dependency])
+      }
+      const module = { exports: {} }
+      factory(localRequire, module, module.exports)
+      return module.exports
+    }
+    require(0)
+  })({ ${modulesString} })`
+}
+
 const result = packing(entry)
+fs.writeFileSync(path.join(output.path, output.filename), result)
 
 console.log(result)
